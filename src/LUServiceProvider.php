423e895f1910bdd6866adf4222b46173a8f89922
bubu2k <marco76tv@hotmail.com>
2017-04-24 14:19:21 +0200

    serv prov

--- src/LUServiceProvider.php
+++ src/LUServiceProvider.php
@@ -1,17 +1,9 @@
 <?php
-<<<<<<< HEAD
 namespace XRA\LU;
-
-=======
-
-namespace XRA\LU;
->>>>>>> 707fd52a55a07887000b4480fcdbc0aab8214a76
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\AliasLoader;
 use XRA\Extend\Traits\ServiceProviderTrait;
-
-
 
 class LUServiceProvider extends ServiceProvider{
      use ServiceProviderTrait;
